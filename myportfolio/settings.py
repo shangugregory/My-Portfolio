8d849eed40f7164524f4cc98e43fc06d7541c136
shangugregory <shangugregory@gmail.com>
2024-03-05 20:12:05 +0300

    preparing for heroku deployment

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6+^n090l)g5gyo_5_cam21ayuok0_$p(rqypnydw+i$pies)1o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
