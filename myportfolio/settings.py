d4f5ab9c38d2830b97963b775afa7211d4d0e3fb
shangugregory <shangugregory@gmail.com>
2024-03-05 19:50:23 +0300

    preparing for heroku deployment

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -12,10 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
