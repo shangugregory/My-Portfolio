91ed7776180856f55e43ce22d4b9313bb173b8ee
shangugregory <shangugregory@gmail.com>
2024-03-05 21:44:15 +0300

    preparing for heroku deployment

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6+^n090l)g5gyo_5_cam21ayuok0_$p(rqypnydw+i$pies)1o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['myportfolio.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
